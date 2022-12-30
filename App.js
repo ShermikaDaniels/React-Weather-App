c1e0f7d8da333cea45fb0a00e565a92bd5c78875
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2022-12-29 23:40:42 -0500

    importing React to VS Code

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <h2>Created By React</h2>
+    </div>
+  );
+}
